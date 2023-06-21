89d1a6058ff8450975919e44b4ba448413c9735e
Jean-Philippe Bernardy <xbejea@merl.clasp.gu.se>
2023-06-21 15:49:13 +0200

    nix-shell

---
+++ shell.nix
@@ -0,0 +1,40 @@
+{ nixpkgs ? import <nixpkgs> {} }:
+let
+  nixpkgs_source = fetchTarball https://github.com/NixOS/nixpkgs/archive/nixos-23.05.tar.gz;
+   overlays = [];
+   config = {
+     allowUnfree = true;
+   };
+   myNix = import nixpkgs_source {inherit overlays; inherit config;};
+in
+with myNix.pkgs; 
+let hp92 = haskell.packages.ghc943;
+    hp = hp92;
+    ghc = hp.ghcWithPackages (ps: with ps; ([
+      cabal-install
+      Agda
+      filemanip
+      pretty
+      containers
+      aeson
+      bytestring
+      mtl
+      directory
+      filepath
+      deepseq
+      async
+    ]));
+    myAgda = agda.withPackages (p: [ p.standard-library ]);
+
+in pkgs.stdenv.mkDerivation {
+  name = "my-env-0";
+  buildInputs = [ glibcLocales zip ghc
+                  # myAgda
+                ]; # git does not work easily here.
+  shellHook = ''
+    export LANG=en_US.UTF-8
+    eval $(egrep ^export ${ghc}/bin/ghc)
+  '';
+}
+
+
