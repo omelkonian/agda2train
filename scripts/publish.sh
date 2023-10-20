484884eeb0e049467a52cb20e1481ed1775df7d3
Orestis Melkonian <melkon.or@gmail.com>
2023-10-20 21:15:09 +0100

    Improve publish.sh script

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -6,21 +6,26 @@ CABAL=$PROJECT.cabal
 curVersion=$(grep "^version" "$CABAL" | cut -d: -f2 | tr -d "[:blank:]")
 prevVersion=$(git show HEAD^:"$CABAL" | grep "^version" | cut -d: -f2 | tr -d "[:blank:]")
 
-[ $curVersion == $prevVersion ]\
-  && echo "Version has not changed, not publishing to Hackage." && exit 1\
+[ $curVersion == $prevVersion ] \
+  && echo "Version has not changed, not publishing to Hackage." && exit 1 \
   || echo "Version has changed, publishing to Hackage."
+
+dir=$(mktemp -d dist.XXXXXX)
+trap 'rm -r "$dir"' EXIT
 
 read -p "Username: " HACKAGE_USERNAME
 read -s -p "Password: " HACKAGE_PASSWORD
 
-cabal sdist
+if [ "$@" != "--only-docs" ]; then
+  echo "Uploading package: $PROJECT-$curVersion"
+  cabal sdist --builddir="$dir"
+  cabal upload -u $HACKAGE_USERNAME -p $HACKAGE_PASSWORD \
+    --publish $dir/sdist/$PROJECT-$curVersion.tar.gz \
+    || echo "** Version $curVersion already uploaded."
+fi
+
+echo "Uploading package documentation: $PROJECT-$curVersion"
+cabal haddock --builddir="$dir" --haddock-for-hackage --haddock-hyperlink-source
 cabal upload -u $HACKAGE_USERNAME -p $HACKAGE_PASSWORD \
-  --publish dist-newstyle/sdist/$PROJECT-$curVersion.tar.gz
-cabal haddock --haddock-for-hackage --haddock-hyperlink-source --haddock-executables
-DOCS=$PROJECT-$curVersion-docs
-tar xvzf dist-newstyle/$DOCS.tar.gz
-mv $DOCS/$PROJECT/* $DOCS/
-rm -r $DOCS/$PROJECT/
-tar cvz --format=ustar -f $DOCS.tar.gz $DOCS/
-cabal upload -u $HACKAGE_USERNAME -p $HACKAGE_PASSWORD \
-  --publish -d $DOCS.tar.gz
+  --publish -d $dir/$PROJECT-$curVersion-docs.tar.gz \
+  || echo "** Documentation for version $curVersion already uploaded."
